46ac8a47c1d0ead6f099e9b4b31e904e315425eb
krizh4 <krishmikaabisheka1@gmail.com>
2023-05-04 00:02:35 +0530

    Initial Commit

---
+++ UI/index.js
@@ -0,0 +1,8 @@
+function course() {
+    // After delay
+    animation-delay: 2000ms;
+    // Change to: "coursenl Frame 3";
+    // Animate: Smart animate;
+    animation-timing-function: ease-in-out;
+    animation-duration: 800ms;
+}
