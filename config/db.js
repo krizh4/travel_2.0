6e60a157a8ab93a017beff5b661e79e1abc33c75
krizh4 <krishmikaabisheka1@gmail.com>
2023-06-01 12:37:03 +0530

    fewChanges

--- config/db.js
+++ config/db.js
@@ -9,6 +9,9 @@ const SiteModels = require('../models/sitemodels')
 
 const mongoose = require('mongoose');
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SGAPI_KEY);
+
 async function run() {
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
